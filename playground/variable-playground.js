ea55edfc98c31e505cfbb6a1f20cdb37e43ff50b
Dean Pillay <ddpillay26@gmail.com>
2016-08-22 22:53:32 +0200

    Init Repo

---
+++ playground/variable-playground.js
@@ -0,0 +1,21 @@
+// var person = {
+// 	name: 'Dean',
+// 	age: 34
+// };
+
+// function updatePerson(obj) {
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [15, 37];
+
+function addGrade(gradesArr) {
+	gradesArr.push(55);
+	debugger;
+}
+
+addGrade(grades);
+console.log(grades);
